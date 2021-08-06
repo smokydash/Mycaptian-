63cff8e714bdab23c93c1d2a2bad98043b928e67
smokydash <88554602+smokydash@users.noreply.github.com>
2021-08-06 23:07:27 +0530

    Created for the practical experience
    The two practicals which was given by the mycaptian team

---
+++ area of the circle.py
@@ -0,0 +1,9 @@
+import math
+radius = float(input("Enter the radius of teh circle : "))
+
+area = math.pi * radius * radius
+
+print("Area of the circle is : {0}".format(area))
+
+
+
---
+++ filename.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 22:55:37 2021
+
+@author: gitam
+"""
+
+filename= input("input filename:")
+f_extns=filename.split(".")
+
+print ("extension of the file is:"+repr(f_extns[-1]))
